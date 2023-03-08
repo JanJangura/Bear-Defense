73b99b049a1dd883d3e8409f9b5d38f83251d207
JanJangura <111806379+JanJangura@users.noreply.github.com>
2023-03-07 21:52:29 -0500

    yes

---
+++ Assets/ScriptableObjects/Bear.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* getters & setters = add security to fields by encapsulation 
+ * They're accessors found within properties
+ * 
+ * properties = combine aspect of both fields and methods (share name with a field)
+ * get accessor = used to return the property value
+ * set accessor = used to assign a new value
+ * value keyword = defines the value being assigned by the set (parameter)
+  
+*/
+
+// Speed is actually set up as an enum
+public enum TowerSpeed
+{
+    Slow,           // By default the first member is 0
+    Medium,         // Set as 1 and so forth
+    Fast,           // Set as 2
+    Hypersonic      // Set as 3
+}
+
+[CreateAssetMenu(fileName = "NewBear", menuName = "ScriptableObjects/Bear")] // To make it possible to create this object throught the create asset menu
+public class Bear : ScriptableObject
+{
+    // The name you want to input in for this game object
+    [SerializeField] private string _name;                  
+
+    // The cost you want this game object to be
+    [SerializeField] private int _cost;
+
+    // The fire rate speed you want this G.O. to be
+    [SerializeField] private TowerSpeed _speed;
+
+    // The Description you would like to have for this G.O.
+    [TextArea][SerializeField] private string _description;
+
+    // To access the data on our Scriptable Objects.
+    public string Name => _name;        
+    public int Cost => _cost;
+
+    // Here is our 
+    public string Speed
+    {
+        get
+        {
+            switch (_speed)
+            {
+                case TowerSpeed.Slow:
+                    return "Slow";
+
+                case TowerSpeed.Medium:
+                    return "Medium";
+
+                case TowerSpeed.Fast:
+                    return "Fast";
+
+                case TowerSpeed.Hypersonic:
+                    return "Hypersonic";
+
+                default:
+                    return "Undefined";
+            }
+        }
+    }
+
+    // To access Description data
+    public string Description => _description;
+}
---
+++ Assets/Scripts/TowersUIController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TowersUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject _towerInformationPanel;
+    [SerializeField] private TMP_Text _towerName;
+    [SerializeField] private TMP_Text _towerCost;
+    [SerializeField] private TMP_Text _towerSpeed;
+    [SerializeField] private TMP_Text _towerDescription;
+
+    void Start()
+    {
+        HideTowerInformation();
+    }
+
+    public void ShowTowerInformation(Bear towerInformation)
+    {
+        _towerInformationPanel.SetActive(true);
+
+        _towerName.text = towerInformation.Name;
+        _towerCost.text = $"<b>Cost:</b> {towerInformation.Cost.ToString()}";
+        _towerSpeed.text = $"<b>Speed:</b> {towerInformation.Speed}";
+        _towerDescription.text = towerInformation.Description;
+    }
+
+    public void HideTowerInformation()
+    {
+        _towerName.text = "";
+        _towerCost.text = "";
+        _towerSpeed.text = "";
+        _towerDescription.text = "";
+
+        _towerInformationPanel.SetActive(false);
+    }
+}
